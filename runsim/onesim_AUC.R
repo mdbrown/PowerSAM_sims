d129d0b9659d4757803def90480de3732ed95f84
mdbrown <mdbrown@fhcrc.org>
2014-03-04 00:14:54 -0800

    update for new round

---
+++ runsim/onesim_AUC.R
@@ -0,0 +1,74 @@
+# expects N and S0
+for(arg in commandArgs(TRUE)) eval(parse(text=arg)) 
+
+
+library(survival)
+source("../../PowerSAM/main.R")
+source("../../PowerSAM/subroutines.R")
+source("../../PowerSAM/Estimation.R")
+
+
+par = "AUC"
+
+t.0 = 3
+a = -log(S0)/t.0
+predict.time = 3
+time.max = 100
+cutoff = 0
+H0 = .6
+Ha = .65
+M = 1000 
+
+
+mybetas <- get.Betas(par, a, predict.time = 5, cutoff = cutoff, H0, Ha)
+beta = mybetas[2]
+ 
+
+#compute lam for censoring distribution
+  mymax = 100-floor(Pr.cTtmax(lam = 100, time.max, a, beta)*100)
+
+  if(cens.perc >mymax){
+    
+    
+    out <- uniroot(function(lam, cens.perc, tmax, a, beta){Pr.cTtmax(lam, tmax, a, beta) - cens.perc}, 
+                   c(0, 100),
+                   cens.perc =  (cens.perc - mymax)/100, 
+                   tmax = time.max, 
+                   a = a, 
+                   beta = beta)$root
+  }else{
+    
+    out = 0
+  }
+  
+  myLam <- out
+  
+
+
+
+
+
+
+tmp <- SimulateN(N, parameter = par, 
+                      S.0 = S0, 
+                      t.0 = t.0,
+                      cutoff= cutoff, 
+                      predict.time = predict.time,
+                      parval.H0 = H0, 
+                      parval.Ha = Ha, 
+                      ESTmethod = "SP", 
+                      f_x = dnorm, 
+                      F_xInv = qnorm, 
+                      mm = M,  
+                      alpha = 0.05, 
+                      cens.lam = myLam, 
+                      time.max = time.max)
+
+
+save(tmp, file = paste("../data/simout_cens_", cens.perc, "_S0_", S0, "_N_", N,"_", par,  ".Rdata", sep = ""))
+
+#library(grid)
+#library(ggplot2)
+#load("../data/simout_S0_0.5_N_100.Rdata")
+
+#printResultPlot(tmp, pars = c("beta", "AUC","TPR", "FPR", "PPV", "NPV"), useLogit=FALSE)
--- runsim/runallsims.py
+++ runsim/runallsims.py
@@ -5,13 +5,14 @@ import commands
 
 def main():
 
-  S0vec = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95]
+  S0vec = [0.80, .85, 0.90, 0.91, 0.92, 0.93, 0.94, 0.95, .96, .97, .98]
   Nvec = [100,  200,  300,  400,  500,  600,  700,  800,  900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000]
+  C = 50.0
 
   for S0 in S0vec:
     for N in Nvec:
 
-      cmd = 'sbatch --account zheng_y --cpus-per-task=1 --time=1-0 --wrap=\"R --no-save --no-restore --args N=%d S0=%f < onesim.R\"' % (N, S0)
+      cmd = 'sbatch --account zheng_y --cpus-per-task=1 --time=1-0 --wrap=\"R --no-save --no-restore --args N=%d S0=%f cens.perc=%f< onesim.R\"' % (N, S0, C)
       (status, output) = commands.getstatusoutput(cmd)
       print(cmd)
       
